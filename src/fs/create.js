230762bf05a53e49503176496a58b1bebb4115c2
GitHubParnoe <zhenyadulyasov@bk.ru>
2024-02-08 11:58:21 +0300

    create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import {promises as fs} from 'fs';
 const create = async () => {
-    // Write your code here 
+   const filePath = './files/fresh.txt';
+   const fileContent = 'I am fresh and young'
+
+   try{
+
+    await fs.access(filePath);
+
+    console.error('File already exists');
+   }
+   catch{
+       try{
+
+        await fs.writeFile(filePath,fileContent);
+        console.log('File created successfully');
+       }catch(error){
+           console.error('Error creating file:',error);
+       }
+   }
 };
 
 await create();
